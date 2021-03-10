d2664a27edc685d1bac29bf657ce1222932c14fa
myrony <myron_yung@sfu.ca>
2021-03-10 09:21:31 -0800

    small test with moving square, added fix to window centering

--- src/main/java/com/group1/Game.java
+++ src/main/java/com/group1/Game.java
@@ -1,29 +1,55 @@
 package com.group1;
 
+import javafx.animation.AnimationTimer;
+import javafx.application.Application;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.Group;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 public class Game {
+    int mv=0;
     public void mouseClick(MouseEvent mouseEvent) {
-
         Group root = new Group();
         Scene game = new Scene(root);
         Canvas canvas = new Canvas(1000,800);
-        root.getChildren().add(canvas);
+        Button b1 = new Button("STOP");
+        root.getChildren().addAll(canvas,b1);
         //get stage from start menu to change scene to game
         Stage window = (Stage)(((Node)mouseEvent.getSource()).getScene().getWindow());
         window.setScene(game);
-        //center game screen(opens off centered without this)
-        Rectangle2D primScreenBounds = Screen.getPrimary().getVisualBounds();
-        window.setX((primScreenBounds.getWidth() - window.getWidth()) / 2);
-        window.setY((primScreenBounds.getHeight() - window.getHeight()) / 2);
+        window.centerOnScreen();
         window.show();
+
+        GameTimer timer = new GameTimer() {
+            @Override
+            public void tick(long now) {
+                GraphicsContext gc = canvas.getGraphicsContext2D();
+                gc.setFill(Color.WHITE);
+                gc.fillRect(0,0,1000,1000);
+                gc.setFill(Color.BLUE);
+                gc.fillRect(500+mv,400,50,50);
+                mv++;
+            }
+        };
+        timer.start();
+
+        b1.setOnAction(e->{
+            if(timer.running){
+                timer.stop();
+                timer.running =false;
+            }
+            else{
+                timer.start();
+            }
+        });
     }
 }
