fed9274a4139713c00482b3a9ea9d90cfcbe9c1d
myrony <myron_yung@sfu.ca>
2021-03-10 17:12:51 -0800

    combined main and start class

--- src/main/java/com/group1/Main.java
+++ src/main/java/com/group1/Main.java
@@ -1,9 +1,34 @@
 package com.group1;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
-public class Main {
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+    Parent startMenu = FXMLLoader.load(getClass().getResource("StartMenu.fxml"));
+    Scene scene1 = new Scene(startMenu);
+    stage.setScene(scene1);
+    stage.show();
+    }
+
     public static void main(String[]args){
-        Application.launch(start.class,args);
+        launch(args);
     }
 }
