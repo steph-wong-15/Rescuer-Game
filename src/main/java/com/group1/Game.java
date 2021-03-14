e790b1d617e6c1895e326bb86878d3d3d4076f01
myrony <myron_yung@sfu.ca>
2021-03-14 13:35:20 -0700

    add pause menu

--- src/main/java/com/group1/Game.java
+++ src/main/java/com/group1/Game.java
@@ -1,10 +1,17 @@
 package com.group1;
 
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 
 public class Game{
@@ -26,15 +33,16 @@ public class Game{
         timer.start();
     }
 
-    public void pauseClicked(MouseEvent mouseEvent) {
-        if(timer!=null){
-            if(timer.running){
-                timer.stop();
-                timer.running =false;
-            }
-            else{
-                timer.start();
-            }
+    public void pauseClicked(MouseEvent mouseEvent) throws IOException {
+        if(timer!=null) {
+//            Parent root = FXMLLoader.load(getClass().getResource("PauseMenu.fxml"));
+//            Scene pauseScreen = new Scene(root);
+//            Stage popWindow = new Stage();
+//            popWindow.setScene(pauseScreen);
+//            popWindow.centerOnScreen();
+//            popWindow.initModality(Modality.APPLICATION_MODAL);
+//            popWindow.show();
+            timer.stop();
         }
     }
 }
