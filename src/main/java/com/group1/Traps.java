07fd1ba1def442493df4874a005ba79bc66b5cd9
jca432 <josh_chen@sfu.ca>
2021-03-21 20:20:42 -0700

    traps java

--- src/main/java/com/group1/Traps.java
+++ src/main/java/com/group1/Traps.java
@@ -1,6 +1,15 @@
+package com.group1;
+
 class Traps extends Enemies{
   private boolean invisible; //set to true or false, sets if the traps are visible to the player or not
   private boolean destroyable; //set to true or false, sets if traps are destroyable with the Axe or not
+
+  public Traps(int health, int[][] pos, int[][] size, float speed, int damageDone, boolean invisible, boolean destroyable) {
+        super(health, pos, size, speed, damageDone);
+        this.invisible = invisible;
+        this.destroyable = destroyable;
+
+    }
 
   public boolean isInvisible(){
     return invisible;
