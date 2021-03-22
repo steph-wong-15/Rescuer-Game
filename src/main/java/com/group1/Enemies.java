4b5c84e338223d2bf4068075c203c76f70a05995
jca432 <josh_chen@sfu.ca>
2021-03-21 20:16:18 -0700

    Enemies commit

--- src/main/java/com/group1/Enemies.java
+++ src/main/java/com/group1/Enemies.java
@@ -1,6 +1,12 @@
+package com.group1;
+
 class Enemies extends Character{
   private int damageDone;
 
+  public Enemies(int health, int[][] pos, int[][] size, float speed, int damageDone) {
+        super(health, pos, size, speed);
+        this.damageDone = damageDone;
+    }
   //return the damageDone
   public int getDamageDone() {
     return damageDone;
