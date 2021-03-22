7f5921aa35b879878de8c8f41ba20794f0a0fe66
jca432 <josh_chen@sfu.ca>
2021-03-21 20:15:03 -0700

    Character commit 2

--- src/main/java/com/group1/Character.java
+++ src/main/java/com/group1/Character.java
@@ -1,7 +1,17 @@
-abstract class Character{ //IMPORTANT: still needs work, did not implement vectors yet and half the modules; fixing next week 
-  private int health;
+package com.group1;
 
+abstract class Character{ 
+  private int health;
+  private int[][] pos;
+  private int[][] size;
   private float speed;
+  
+  public Character(int health, int[][] pos, int[][] size, float speed) {
+        this.health = health;
+        this.pos= pos;
+        this.speed = speed;
+        this.size = size;
+    }
 
   //return the health
   public int getHealth() {
@@ -19,9 +29,38 @@ abstract class Character{ //IMPORTANT: still needs work, did not implement vecto
   public void characterCollision(Character c){
     //implement characterCollision
   }
+  public void wallCollision(Wall w){
+    //implement wallCollision
+  }
+
+  public int[][] getPos() {
+      return pos;
+  }
+
+  public void setPos(int[][] pos) {
+      this.pos = pos;
+  }
+
+
+  public int[][] getSize() {
+        return size;
+    }
+
+  public void setSize(int[][] size) {
+      this.size = size;
+  }
+
+  public float getSpeed() {
+    return speed;
+  }
+
+  //set the damageDone
+  public void setSpeed(float speed){
+    this.speed = speed;
+  } 
 
   public void draw(){
     //implement a drawing function
   }
-
+  
 }
