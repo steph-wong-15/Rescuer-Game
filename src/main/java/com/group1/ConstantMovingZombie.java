58464db4bcf699aaaa1d43c960e5ae893a9c8483
jca432 <josh_chen@sfu.ca>
2021-03-21 20:15:58 -0700

    ConstantMovingZombie commit

--- src/main/java/com/group1/ConstantMovingZombie.java
+++ src/main/java/com/group1/ConstantMovingZombie.java
@@ -1,5 +1,12 @@
+package com.group1;
+
 class ConstantMovingZombie extends Moving{
   private String movementPattern; //possible values: vertical, horizontal, or diagonal
+
+  public ConstantMovingZombie(int health, int[][] pos, int[][] size, float speed, int damageDone, int range, String movementPattern) {
+        super(health, pos, size, speed, damageDone, range);
+        this.movementPattern = movementPattern;
+  }
 
   //return the damageDone
   public String getMovementPattern() {
