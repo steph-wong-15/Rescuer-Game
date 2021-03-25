9d9a1f6f46a67404b4b229bb56eaa4023634b21e
jca432 <josh_chen@sfu.ca>
2021-03-24 19:52:50 -0700

    temporary changes, game.java definitely still needs work. old game.java still in file history

--- src/main/java/com/group1/Settings.java
+++ src/main/java/com/group1/Settings.java
@@ -3,10 +3,10 @@ package com.group1;
 public class Settings {
 
     public static double SCENE_WIDTH = 400;
-    public static double SCENE_HEIGHT = 800;
+    public static double SCENE_HEIGHT = 400;
 
     public static double PLAYER_SPEED = 4.0;
-    public static double PLAYER_HEALTH = 100.0;
+    public static int PLAYER_HEALTH = 10;
 
     public static int ENEMY_SPAWN_RANDOMNESS = 100;
 
