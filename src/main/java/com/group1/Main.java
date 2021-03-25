d9c2333f23abefc270db09249c05ed158e9c573f
myrony <myron_yung@sfu.ca>
2021-03-24 21:18:19 -0700

    removed settings button

--- src/main/java/com/group1/Main.java
+++ src/main/java/com/group1/Main.java
@@ -2,15 +2,14 @@ package com.group1;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 
 public class Main extends Application {
-
     @Override
     public void start(Stage stage) throws Exception {
     Parent startMenu = FXMLLoader.load(getClass().getResource("StartMenu.fxml"));
