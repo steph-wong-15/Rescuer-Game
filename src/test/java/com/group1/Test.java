b54651547d8ea00c4810be7564b4e9aa78dd4e86
jca432 <josh_chen@sfu.ca>
2021-04-12 16:13:08 -0700

    Basic settings commit, checks that all the settings variables are up to standard

---
+++ src/test/java/com/group1/Test.java
@@ -0,0 +1,70 @@
+package com.group1;
+
+import javafx.scene.image.Image;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Juniper Initial Class
+ * Mere check to see if tests function properly
+ */
+class JUnitJupiterInitializeTest{
+    int jupiter = 0;
+    @Test
+    void addition() {
+        assertEquals(0, jupiter);
+    }
+}
+
+/**
+ * PlayerSettingsTest Class
+ * Tests all the params in Settings.java to see if they are the right values
+ * Values must be at the right constants or tests will fail
+ */
+class PlayerSettingsTest{
+
+    private final double widthCheck = Settings.SCENE_WIDTH;
+    private final double heightCheck = Settings.SCENE_HEIGHT;
+    private final double speedCheck = Settings.PLAYER_SPEED;
+    private final int healthCheck = Settings.PLAYER_HEALTH;
+    private final double spawnCheck = Settings.ENEMY_SPAWN_RANDOMNESS;
+
+    /**
+     * Settings variables, equal to the values in Settings
+     * widthCheck must be equal to 750.0 as a double
+     * heightCheck must be equal to 500.0 as a double
+     * speedCheck must be equal to 3.0 as a double
+     * healthCheck must be equal to 3 as an int
+     * spawnCheck must be equal to 30 as a double
+     */
+
+    @Test
+    void widthChecker() {
+        assertEquals(750.0, widthCheck);
+    }
+
+    @Test
+    void heightChecker() {
+        assertEquals(500.0, heightCheck);
+    }
+
+    @Test
+    void speedChecker() {
+        assertEquals(3.0, speedCheck);
+    }
+
+    @Test
+    void healthChecker() {
+        assertEquals(3, healthCheck);
+    }
+
+    @Test
+    void spawnChecker() {
+        assertEquals(30, spawnCheck);
+    }
+}
+
+
