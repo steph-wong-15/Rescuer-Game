42ecf5fceb38636223583c5adc75b287efc4e470
Stephanie <dwa102@sfu.ca>
2021-04-14 20:39:57 -0700

    added wall like structures

---
+++ src/main/java/com/group1/Unbreakable.java
@@ -0,0 +1,13 @@
+package com.group1;
+
+import javafx.scene.image.Image;
+import javafx.scene.layout.Pane;
+
+public class Unbreakable extends Person{
+
+
+    public Unbreakable(Image image, Pane layer, double x, double y) {
+        super(image, layer, x, y);
+    }
+
+}
