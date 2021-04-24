dc2c23ecb04cd026c84751501b6d6e11271d85d7
myrony <myron_yung@sfu.ca>
2021-04-23 23:54:13 -0700

    added dummy class

---
+++ src/main/java/com/group1/dummy.java
@@ -0,0 +1,12 @@
+package com.group1;
+
+public class dummy {
+    /**
+     * Used to get around "JavaFX runtime components are missing" error when executing jar file
+     */
+
+    public static void main(String[] args){
+        Main.main(args);
+    }
+}
+
