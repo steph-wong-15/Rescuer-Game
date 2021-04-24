3c632e4b61a640ee4adcb6074a7394bfd3f8b842
myrony <myron_yung@sfu.ca>
2021-04-23 23:59:16 -0700

    changed main class

--- src/main/java/com/group1/dummy.java
+++ src/main/java/com/group1/dummy.java
@@ -2,7 +2,7 @@ package com.group1;
 
 public class dummy {
     /**
-     * Used to get around "JavaFX runtime components are missing" error when executing jar file
+     * Used to get around "JavaFX runtime components are missing" error when executing jar file.
      */
 
     public static void main(String[] args){
